cefc0119950fb96939d1f0241caccf4787abeb70
D00225745 <D00225745@student.dkit.ie>
2023-04-14 20:01:18 +0100

    Occlusion
    Baked the occlusion, and now trying to solve occlusion portal errors

--- Assets/My Assets/Scripts/Occlusion/OcclusionPortal.cs
+++ Assets/My Assets/Scripts/Occlusion/OcclusionPortal.cs
@@ -2,17 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class OcclusionPortal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Instruction;
+    public GameObject AnimeObject;
+    public GameObject ThisTrigger;
+    public AudioSource DoorOpenSound;
+    public bool Action = false;
+
     void Start()
     {
+        Instruction.SetActive(false);
+
         
+
     }
 
-    // Update is called once per frame
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Instruction.SetActive(true);
+            Action = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collision)
+    {
+        Instruction.SetActive(false);
+        Action = false;
+    }
+
+
     void Update()
     {
         
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Action == true)
+            {
+                Instruction.SetActive(false);
+                AnimeObject.GetComponent<Animator>().Play("DoorOpen");
+                ThisTrigger.SetActive(false);
+                DoorOpenSound.Play();
+                Action = false;
+            }
+        }
+
     }
 }
